046097523c599075446938c234733f00699861c0
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-10-25 16:27:22 +0900

    chore:スクリプト追加

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,15 +1,17 @@
 const { series, parallel } = require('gulp');
 const core = require('./ops/gulp/tasks/core');
+const custom = require('./ops/gulp/tasks/custom');
 
 exports.default = series(
     core.webpackBuildTasks(),
     parallel(
         core.asciidoctorBuildTasks(),
         core.marpBuildTasks(),
+        custom.asciidoctorBuildTasks(),
     ),
     series(
         parallel(core.webpack.server, core.asciidoctor.server),
-        parallel(core.webpack.watch, core.asciidoctor.watch, core.marp.watch),
+        parallel(core.webpack.watch, core.asciidoctor.watch, core.marp.watch,custom.asciidoctor.watch),
     ),
 );
 
@@ -18,12 +20,13 @@ exports.build = series(
     parallel(
         core.asciidoctorBuildTasks(),
         core.marpBuildTasks(),
+        custom.asciidoctorBuildTasks(),
     )
 );
 
 exports.docs = series(
     parallel(core.asciidoctorBuildTasks(), core.marpBuildTasks()),
-    parallel(core.asciidoctor.server, core.asciidoctor.watch, core.marp.watch),
+    parallel(core.webpack.watch, core.asciidoctor.watch, core.marp.watch,custom.asciidoctor.watch),
 );
 exports.slides = series(core.marp.build);
 
---
+++ ops/gulp/tasks/custom.js
@@ -0,0 +1,55 @@
+const tasks =  {
+    task1: (cb) => {
+        // place code for your default task here
+        cb();
+    }
+}
+
+exports.tasks = tasks;
+
+const { series, watch, src, dest } = require('gulp');
+const fs = require('fs-extra');
+const kroki = require('asciidoctor-kroki');
+const browserSync = require('browser-sync').create();
+
+const asciidoctor = {
+    clean: async (cb) => {
+        await fs.remove("./public/docs/case"); // fs-extraでディレクトリを非同期で削除
+        cb(); // コールバック関数を呼び出す
+    },
+    build: (cb) => {
+        const asciidoctor = require("@asciidoctor/core")();
+        const krokiRegister = () => {
+            const registry = asciidoctor.Extensions.create();
+            kroki.register(registry);
+            return registry;
+        };
+
+        const inputRootDir = "./docs/case";
+        const outputRootDir = "./public/docs/case";
+        const fileNameList = fs.readdirSync(inputRootDir);
+        const docs = fileNameList.filter(RegExp.prototype.test, /.*\.adoc$/);
+
+        docs.map((input) => {
+            const file = `${inputRootDir}/${input}`;
+            asciidoctor.convertFile(file, {
+                safe: "safe",
+                extension_registry: krokiRegister(),
+                to_dir: outputRootDir,
+                mkdirs: true,
+            });
+        });
+        src(`${inputRootDir}/images/*.*`).pipe(dest(`${outputRootDir}/images`))
+            .on('end', cb); // src.pipeの完了後にcb()を実行
+    },
+    watch: (cb) => {
+        watch("./docs/**/*.adoc", asciidoctor.build);
+        cb();
+    },
+}
+
+exports.asciidoctor = asciidoctor;
+exports.asciidoctorBuildTasks = () => {
+    return series(asciidoctor.clean, asciidoctor.build);
+}
+
